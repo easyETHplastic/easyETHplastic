dde787a5bd35c51468f8abb1ff39be1e34da45b2
Reuben Rodrigues <reubenrodrigues24@gmail.com>
2022-09-03 12:14:49 +0200

    contracts: nonce

--- contracts/src/PlasticETH.sol
+++ contracts/src/PlasticETH.sol
@@ -4,13 +4,15 @@ pragma solidity ^0.8.16;
 import "@solmate/auth/Owned.sol";
 
 contract PlasticETH is Owned {
-    event Buy(address indexed buyer, uint256 amount);
+    event Buy(address indexed buyer, uint256 amount, uint256 nonce);
+
+    uint256 private nonce;
 
     constructor() Owned(msg.sender) {}
 
-    function buy() external payable returns (uint256) {
-        emit Buy(msg.sender, msg.value);
-        return msg.value;
+    function buy() external payable returns (uint256 nonce_) {
+        nonce_ = nonce++;
+        emit Buy(msg.sender, msg.value, nonce_);
     }
 
     function withdraw() external onlyOwner {
